74b3534c011aff87f2dafea041dd317ac110e29e
Scott Davis <stdavis@utah.gov>
2019-09-26 15:38:55 -0600

    fix: fix order of fields in Station.csv download file #5

--- scripts/sql/Location.sql
+++ scripts/sql/Location.sql
@@ -1,17 +1,17 @@
 SELECT
   se.EVENT_ID,
   se.EVENT_DATE,
+  s.NAME,
+  s.STATION_ID,
+  s.STREAM_TYPE,
   se.GEO_DEF,
   se.LOCATION_NOTES,
   se.SEGMENT_LENGTH,
   se.NUM_PASSES,
   se.WEATHER,
   se.OBSERVERS,
-  se.SURVEY_PURPOSE,
+  se.SURVEY_PURPOSE
 
-  s.NAME,
-  s.STATION_ID,
-  s.STREAM_TYPE
 FROM {0}.WILDADMIN.SamplingEvents_evw as se
 
 INNER JOIN {0}.WILDADMIN.Stations_evw as s
