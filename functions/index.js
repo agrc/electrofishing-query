603591429ab667c2db1e03168a10e9c2f73a7d57
stdavis <stdavis@utah.gov>
2023-03-28 12:39:24 -0600

    feat: implement security via UtahID/Firebase
    Closes #67

---
+++ functions/index.js
@@ -0,0 +1,47 @@
+import 'dotenv/config';
+import initProxy from 'firebase-auth-arcgis-server-proxy';
+import functions from 'firebase-functions';
+
+const options = {
+    host: process.env.AGS_HOST,
+    mappings: [
+        {
+            from: /^\/toolbox/,
+            to: '/arcgis/rest/services/Electrofishing/Download/GPServer',
+            secrets: 'ARCGIS_SERVER_CREDENTIALS'
+        },
+        {
+            from: /^\/mapservice/,
+            to: '/arcgis/rest/services/Electrofishing/MapService/FeatureServer',
+            secrets: 'ARCGIS_SERVER_CREDENTIALS'
+        },
+        {
+            from: /^\/reference/,
+            to: '/arcgis/rest/services/Electrofishing/Reference/MapServer',
+            secrets: 'ARCGIS_SERVER_CREDENTIALS'
+        }
+    ],
+    claimsCheck: (claims) => {
+        if (process.env.FUNCTIONS_EMULATOR) {
+            return true;
+        }
+
+        const utahIDEnvironments = {
+            prod: 'Prod',
+            stage: 'AT',
+            development: 'Dev'
+        };
+
+        return (
+            claims.firebase?.sign_in_attributes?.['DWRElectroFishing:AccessGranted'] &&
+      claims.firebase.sign_in_attributes['DWRElectroFishing:AccessGranted'].includes(
+          utahIDEnvironments[process.env.ENVIRONMENT]
+      )
+        );
+    }
+};
+
+const [proxy, secrets] = initProxy(options);
+
+export const maps = functions.runWith({ secrets,
+    invoker: 'public' }).https.onRequest(proxy);
