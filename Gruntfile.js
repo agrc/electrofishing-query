64bdb2dd9a2d5f61ca4fca30bf4e32cb4ca49da3
stdavis <stdavis@utah.gov>
2023-03-28 16:00:26 -0600

    fix: include service worker in build

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,7 +65,7 @@ module.exports = function (grunt) {
                     {
                         expand: true,
                         cwd: 'src/',
-                        src: ['*.html'],
+                        src: ['*.html', 'ServiceWorker.js'],
                         dest: 'dist/'
                     }
                 ]
