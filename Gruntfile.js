816c8dd8f91ecf1682ddb8cd855b3e10a9248ac8
stdavis <stdavis@utah.gov>
2023-03-28 13:21:48 -0600

    fix: don't create commit or tag on bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,8 +47,9 @@ module.exports = function (grunt) {
         bump: {
             options: {
                 files: bumpFiles,
-                commitFiles: bumpFiles.concat(['_src/ChangeLog.html']),
-                push: false
+                push: false,
+                commit: false,
+                createTag: false
             }
         },
         clean: {
