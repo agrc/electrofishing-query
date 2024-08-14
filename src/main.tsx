b0bd4983a4155eb56d18ee9b12d222dcedf280dc
steveoh <sgourley@utah.gov>
2024-08-13 18:11:10 -0600

    chore: create basic layout

---
+++ src/main.tsx
@@ -0,0 +1,32 @@
+import '@arcgis/core/assets/esri/themes/light/main.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { AnalyticsProvider, FirebaseAppProvider, MapProvider } from './components/contexts';
+import './index.css';
+
+let firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
+};
+
+if (import.meta.env.VITE_FIREBASE_CONFIG) {
+  firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
+}
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <AnalyticsProvider>
+        <MapProvider>
+          <App />
+        </MapProvider>
+      </AnalyticsProvider>
+    </FirebaseAppProvider>
+  </React.StrictMode>,
+);
