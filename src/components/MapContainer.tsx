61b59cfb78b8147427af383e0ad0c4eb84b49c21
steveoh <sgourley@utah.gov>
2024-08-13 18:11:10 -0600

    chore: create basic layout

---
+++ src/components/MapContainer.tsx
@@ -0,0 +1,110 @@
+import Polygon from '@arcgis/core/geometry/Polygon';
+import VectorTileLayer from '@arcgis/core/layers/VectorTileLayer';
+import EsriMap from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+import LayerSelector from '@ugrc/layer-selector';
+import { useEffect, useRef, useState } from 'react';
+import cityExtents from './data/cityExtents';
+import { useMap } from './hooks';
+import { randomize } from './utils';
+
+import '@ugrc/layer-selector/src/LayerSelector.css';
+
+const { item: randomExtent } = randomize<__esri.GraphicProperties>(cityExtents);
+const urls = {
+  landownership:
+    'https://gis.trustlands.utah.gov/hosting/rest/services/Hosted/Land_Ownership_WM_VectorTile/VectorTileServer',
+};
+
+type LayerFactory = {
+  Factory: new () => __esri.Layer;
+  url: string;
+  id: string;
+  opacity: number;
+};
+type SelectorOptions = {
+  view: MapView;
+  quadWord: string;
+  baseLayers: Array<string | { token: string; selected: boolean } | LayerFactory>;
+  overlays?: Array<string | LayerFactory>;
+  position: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+};
+
+export const MapContainer = ({ onClick }: { onClick?: __esri.ViewImmediateClickEventHandler }) => {
+  const mapNode = useRef<HTMLDivElement | null>(null);
+  const mapComponent = useRef<EsriMap | null>(null);
+  const mapView = useRef<MapView>();
+  const clickHandler = useRef<IHandle>();
+  const [selectorOptions, setSelectorOptions] = useState<SelectorOptions | null>(null);
+  console.log('rendering MapContainer');
+  const { setMapView } = useMap();
+
+  // setup the Map
+  useEffect(() => {
+    if (!mapNode.current || !setMapView) {
+      return;
+    }
+
+    mapComponent.current = new EsriMap({
+      basemap: {},
+    });
+
+    mapView.current = new MapView({
+      container: mapNode.current,
+      map: mapComponent.current,
+      extent: new Polygon(randomExtent.geometry).extent,
+      ui: {
+        components: ['zoom'],
+      },
+    });
+
+    setMapView(mapView.current);
+
+    const selectorOptions: SelectorOptions = {
+      view: mapView.current,
+      quadWord: import.meta.env.VITE_DISCOVER_QUAD_WORD,
+      baseLayers: ['Hybrid', 'Lite', 'Terrain', 'Topo', 'Color IR'],
+      overlays: [
+        'Address Points',
+        {
+          Factory: VectorTileLayer,
+          url: urls.landownership,
+          id: 'Land Ownership',
+          opacity: 0.3,
+        },
+      ],
+      position: 'top-right',
+    };
+
+    const { index: randomBaseMapIndex } = randomize(selectorOptions.baseLayers);
+
+    selectorOptions.baseLayers[randomBaseMapIndex] = {
+      token: selectorOptions.baseLayers[randomBaseMapIndex] as string,
+      selected: true,
+    };
+
+    setSelectorOptions(selectorOptions);
+
+    return () => {
+      mapView.current?.destroy();
+      mapComponent.current?.destroy();
+    };
+  }, [setMapView]);
+
+  // add click event handlers
+  useEffect(() => {
+    if (onClick) {
+      clickHandler.current = mapView.current!.on('immediate-click', onClick);
+    }
+
+    return () => {
+      clickHandler.current?.remove();
+    };
+  }, [onClick, mapView]);
+
+  return (
+    <div ref={mapNode} className="size-full">
+      {selectorOptions?.view && <LayerSelector {...selectorOptions}></LayerSelector>}
+    </div>
+  );
+};
