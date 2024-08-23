493c6870b9346d57f3db41175e305a5bf0ab8cd1
stdavis <stdavis@utah.gov>
2024-08-23 12:54:22 -0600

    feat: add root-level error fallback

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import '@arcgis/core/assets/esri/themes/light/main.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import App from './App';
 import { AnalyticsProvider, FirebaseAppProvider, MapProvider } from './components/contexts';
 import './index.css';
@@ -19,14 +20,30 @@ if (import.meta.env.VITE_FIREBASE_CONFIG) {
   firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
 }
 
+const MainErrorFallback = ({ error, resetErrorBoundary }: { error: Error; resetErrorBoundary: () => void }) => {
+  return (
+    <div className="static flex h-screen w-screen items-center justify-center">
+      <div className="flex-col items-center">
+        <h1>Something went wrong</h1>
+        <pre style={{ color: 'red' }}>{error.message}</pre>
+        <button className="w-full rounded-full border p-1" onClick={resetErrorBoundary}>
+          Try again
+        </button>
+      </div>
+    </div>
+  );
+};
+
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-      <AnalyticsProvider>
-        <MapProvider>
-          <App />
-        </MapProvider>
-      </AnalyticsProvider>
-    </FirebaseAppProvider>
+    <ErrorBoundary FallbackComponent={MainErrorFallback} onReset={() => window.location.reload()}>
+      <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+        <AnalyticsProvider>
+          <MapProvider>
+            <App />
+          </MapProvider>
+        </AnalyticsProvider>
+      </FirebaseAppProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
