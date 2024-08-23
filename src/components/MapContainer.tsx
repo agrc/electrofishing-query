c6a257dd6a59cb32e9e251a5c2214a95fd95fd67
stdavis <stdavis@utah.gov>
2024-08-23 16:18:53 -0600

    fix: get rid of "failed to load basemap" console error from esri js

--- src/components/MapContainer.tsx
+++ src/components/MapContainer.tsx
@@ -52,9 +52,7 @@ export const MapContainer = ({ onClick }: { onClick?: __esri.ViewImmediateClickE
       return;
     }
 
-    mapComponent.current = new EsriMap({
-      basemap: {},
-    });
+    mapComponent.current = new EsriMap();
 
     mapView.current = new MapView({
       container: mapNode.current,
