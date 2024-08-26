bb7ca66e45660df7e73986b31b96a8c59d1984b4
stdavis <stdavis@utah.gov>
2024-08-26 13:21:09 -0600

    chore: use tailwind

--- src/ErrorFallback.tsx
+++ src/ErrorFallback.tsx
@@ -2,7 +2,7 @@ export default function ErrorFallback({ error }: { error: Error }) {
   return (
     <div role="alert">
       <p>Something went wrong:</p>
-      <pre style={{ color: 'red' }}>{error.message}</pre>
+      <pre className="text-red-500">{error.message}</pre>
     </div>
   );
 }
