3f882d73c816bf45198261014bf6e22049afaf0b
stdavis <stdavis@utah.gov>
2024-09-06 13:41:15 -0600

    chore: switch out local for design system firebase providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import '@arcgis/core/assets/esri/themes/light/main.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
+import { FirebaseAnalyticsProvider, FirebaseAppProvider, FirebaseAuthProvider } from '@ugrc/utah-design-system';
+import { OAuthProvider } from 'firebase/auth';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ErrorBoundary } from 'react-error-boundary';
 import App from './App';
-import { AnalyticsProvider, FirebaseAppProvider, MapProvider } from './components/contexts';
+import { MapProvider } from './components/contexts/MapProvider';
 import './index.css';
 
 let firebaseConfig = {
@@ -20,6 +22,8 @@ let firebaseConfig = {
 if (import.meta.env.VITE_FIREBASE_CONFIG) {
   firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
 }
+
+const provider = new OAuthProvider('oidc.utah-id');
 
 const MainErrorFallback = ({ error, resetErrorBoundary }: { error: Error; resetErrorBoundary: () => void }) => {
   return (
@@ -38,17 +42,19 @@ const MainErrorFallback = ({ error, resetErrorBoundary }: { error: Error; resetE
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <StrictMode>
     <ErrorBoundary FallbackComponent={MainErrorFallback} onReset={() => window.location.reload()}>
-      <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-        <AnalyticsProvider>
-          <QueryClientProvider client={queryClient}>
-            <MapProvider>
-              <App />
-            </MapProvider>
-          </QueryClientProvider>
-        </AnalyticsProvider>
+      <FirebaseAppProvider config={firebaseConfig}>
+        <FirebaseAuthProvider provider={provider}>
+          <FirebaseAnalyticsProvider>
+            <QueryClientProvider client={queryClient}>
+              <MapProvider>
+                <App />
+              </MapProvider>
+            </QueryClientProvider>
+          </FirebaseAnalyticsProvider>
+        </FirebaseAuthProvider>
       </FirebaseAppProvider>
     </ErrorBoundary>
-  </React.StrictMode>,
+  </StrictMode>,
 );
