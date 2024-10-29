bb48b6c50cbd60385dbea4bcbc57ca350f66aa43
stdavis <stdavis@utah.gov>
2024-10-29 09:39:41 -0600

    chore: fix unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
 import loadVersion from 'vite-plugin-package-version';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), loadVersion()],
+  test: {
+    server: {
+      deps: {
+        // this package does not export esm that uses imports with the file extension, this is a workaround
+        // to instruct vitest to bundle the package similar to the app
+        // https://stackoverflow.com/questions/77126321/how-to-get-vitest-working-with-require-of-es-module
+        inline: ['@ugrc/utah-design-system'],
+      },
+    },
+  },
 });
