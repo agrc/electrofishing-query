34ee5952b9310939b4fc8ef889e133f58b05e09b
stdavis <stdavis@utah.gov>
2024-10-30 09:37:02 -0600

    chore: bump ugrc packages and remove vitest config workaround

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,4 @@ import { defineConfig } from 'vitest/config';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), loadVersion()],
-  test: {
-    server: {
-      deps: {
-        // this package does not export esm that uses imports with the file extension, this is a workaround
-        // to instruct vitest to bundle the package similar to the app
-        // https://stackoverflow.com/questions/77126321/how-to-get-vitest-working-with-require-of-es-module
-        inline: ['@ugrc/utah-design-system'],
-      },
-    },
-  },
 });
