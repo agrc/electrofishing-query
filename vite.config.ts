f5664012da86c2ce08b755ff4ecf4948366157d7
stdavis <stdavis@utah.gov>
2024-10-30 15:49:16 -0600

    chore: use jsdom for tests

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import { defineConfig } from 'vitest/config';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), loadVersion()],
+  test: {
+    environment: 'jsdom',
+  },
 });
