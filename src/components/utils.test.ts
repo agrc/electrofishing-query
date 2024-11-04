84a5313db100d81ff1b1ab028ffa4f5f9c7587dd
stdavis <stdavis@utah.gov>
2024-11-04 15:29:20 -0700

    feat: wire up selection to download panel

--- src/components/utils.test.ts
+++ src/components/utils.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import config from '../config';
 import { Result } from './ResultsGrid';
-import { getResultOidsFromStationIds, getStationIdsFromResultRows } from './utils';
+import { getEventIdsForDownload, getResultOidsFromStationIds, getStationIdsFromResultRows } from './utils';
 
 describe('getStationIdsFromResultRows', () => {
   const data: Result[] = [
@@ -38,5 +38,36 @@ describe('getResultOidsFromStationIds', () => {
     const selectedStationIds = new Set(['C']);
     const result = getResultOidsFromStationIds(data, selectedStationIds);
     expect(result).toEqual(new Set());
+  });
+});
+
+describe('getEventIdsForDownload', () => {
+  const data: Result[] = [
+    { [config.fieldNames.ESRI_OID]: '1', [config.fieldNames.EVENT_ID]: 'E1' },
+    { [config.fieldNames.ESRI_OID]: '2', [config.fieldNames.EVENT_ID]: 'E2' },
+  ];
+
+  it('should return all event IDs if selectedKeys is "all"', () => {
+    const selectedKeys = 'all';
+    const result = getEventIdsForDownload(data, selectedKeys);
+    expect(result).toEqual(['E1', 'E2']);
+  });
+
+  it('should return all event IDs if selectedKeys is an empty set', () => {
+    const selectedKeys: Set<string> = new Set();
+    const result = getEventIdsForDownload(data, selectedKeys);
+    expect(result).toEqual(['E1', 'E2']);
+  });
+
+  it('should return correct event IDs for selected OIDs', () => {
+    const selectedKeys = new Set(['1']);
+    const result = getEventIdsForDownload(data, selectedKeys);
+    expect(result).toEqual(['E1']);
+  });
+
+  it('should return an empty array if no matching OIDs', () => {
+    const selectedKeys = new Set(['3']);
+    const result = getEventIdsForDownload(data, selectedKeys);
+    expect(result).toEqual([]);
   });
 });
