b16c543e8548471e3c5d043219d30ca20c5f5a7f
stdavis <stdavis@utah.gov>
2025-01-28 07:12:10 -0700

    Revert "fix: switch to clickable download link"
    This reverts commit e9656472179110555b2a3d8c8839e360d9fd01e5.

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -1,6 +1,6 @@
 import { submitJob } from '@arcgis/core/rest/geoprocessor';
 import { useQueryClient } from '@tanstack/react-query';
-import { Button, Link, Select, SelectItem, Spinner } from '@ugrc/utah-design-system';
+import { Button, Select, SelectItem, Spinner } from '@ugrc/utah-design-system';
 import { useState } from 'react';
 import config from '../config';
 
@@ -22,7 +22,6 @@ type State = {
   format: string;
   error: string | null;
   isBusy: boolean;
-  url: string | null;
 };
 
 export default function Download({ eventIds }: { eventIds: string[] }) {
@@ -31,7 +30,6 @@ export default function Download({ eventIds }: { eventIds: string[] }) {
     format: '',
     error: null,
     isBusy: false,
-    url: null,
   });
 
   const updateState = (newState: Partial<State>) => setState((prev) => ({ ...prev, ...newState }));
@@ -58,14 +56,14 @@ export default function Download({ eventIds }: { eventIds: string[] }) {
       return;
     }
 
-    // const link = document.createElement('a');
-    // link.href = url;
-    // link.download = 'data.zip';
-    // document.body.appendChild(link);
-    // link.click();
-    // // document.body.removeChild(link);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'data.zip';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 
-    updateState({ isBusy: false, url });
+    updateState({ isBusy: false });
   };
 
   return (
@@ -91,20 +89,9 @@ export default function Download({ eventIds }: { eventIds: string[] }) {
           variant="secondary"
           onPress={onDownloadClick}
         >
-          {state.isBusy ? (
-            <Spinner />
-          ) : (
-            `Generate download of ${eventIds.length} record${eventIds.length > 1 ? 's' : ''}`
-          )}
+          {state.isBusy ? <Spinner /> : `Download ${eventIds.length} record${eventIds.length > 1 ? 's' : ''}`}
         </Button>
       </p>
-      {state.url && (
-        <p className="my-4 text-lg">
-          <Link href={state.url} download="data.zip">
-            Download data
-          </Link>
-        </p>
-      )}
       {state.error && <div className="text-sm text-rose-600 forced-colors:text-[Mark]">{state.error}</div>}
     </div>
   );
