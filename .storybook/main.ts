6d8a875d7c8ab4739b1b7205b4905fca94e5b0b4
stdavis <stdavis@utah.gov>
2025-07-30 09:03:29 -0600

    chore: upgrade storybook and related deps

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,20 +2,16 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@chromatic-com/storybook'
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs'
   ],
 
   framework: {
     name: '@storybook/react-vite',
     options: {},
-  },
-
-  docs: {
-    autodocs: true
   },
 
   typescript: {
